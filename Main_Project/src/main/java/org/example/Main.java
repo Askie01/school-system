c29df1040c39a910ecf3220ede8004d286e08950
Askie01 <133720459+Askie01@users.noreply.github.com>
2023-07-16 19:17:09 +0200

    Update Main.java
    Added 2 students in the Main file

--- Main_Project/src/main/java/org/example/Main.java
+++ Main_Project/src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Student student1 = new Student("Michal", "Kowalski", 21, 12345);
+        Student student2 = new Student("Ania", "Kowalska", 19, 54321);
     }
 }
