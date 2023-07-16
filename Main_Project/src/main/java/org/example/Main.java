d4a6b554618b511de0ee7bf64d89e3f0e1a949a3
Askie01 <adam.paw01@gmail.com>
2023-07-16 19:20:49 +0200

    Added student3 and student4 in the Main file

--- Main_Project/src/main/java/org/example/Main.java
+++ Main_Project/src/main/java/org/example/Main.java
@@ -3,7 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Student student1 = new Student("Michal", "Kowalski", 21, 12345);
-        Student student2 = new Student("Ania", "Kowalska", 19, 54321);
+        Student student1 = new Student("Michal", "Kowalski", 19, 12345);
+        Student student2 = new Student("Ania", "Kowalska", 20, 54321);
+        Student student3 = new Student("Kamil", "Szymaniec", 21, 6789);
+        Student student4 = new Student("Kasia", "Chojnacka", 22, 9876);
     }
 }
