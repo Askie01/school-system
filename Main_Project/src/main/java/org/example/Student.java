0f498534c0a883c0f8ad602e0dcaf9199d7d72d2
Askie01 <adam.paw01@gmail.com>
2023-07-16 18:38:25 +0200

    Added student class

---
+++ Main_Project/src/main/java/org/example/Student.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private final int ID;
+
+    public Student(String firstName, String lastName, int age, int ID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getID() {
+        return ID;
+    }
+}
